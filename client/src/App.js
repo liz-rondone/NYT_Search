6a474467a2d2277a1255fa70a25329cf80e4722b
liz-rondone <liz.rondone@northwestern.edu>
2017-10-06 18:29:45 -0500

    mern foundation

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route } from "react-router-dom"
-import Home from "./components/Home"
-import User from "./components/User"
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/users" component={User} />
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
         </div>
-      </Router>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
