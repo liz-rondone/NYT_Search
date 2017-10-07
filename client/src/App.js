3ff1020c55a725415fc307f1c31b99dd556bcc12
liz-rondone <liz.rondone@northwestern.edu>
2017-10-06 20:29:20 -0500

    working foundation

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route } from "react-router-dom"
+import Home from "./components/Home"
+import User from "./components/User"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/users" component={User} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
